3cf904cab8196bb070c35ed3cc45aafacc3e0687
Lewin Kelly <heli@odex.cf>
2025-06-10 05:28:27 +0100

    Better log storage and error management

--- main.go
+++ main.go
@@ -1,29 +1,35 @@
 package main
 
 import (
+	"crypto/sha3"
+	"encoding/hex"
 	"fmt"
 	"os"
-	"time"
+	"path/filepath"
 )
 
-func cmd() {
-	// current process id
-	pid := os.Getpid()
-	fmt.Println("Current process ID:", pid)
+const logsDir = "./logs"
 
-	outfile, err := os.Create(fmt.Sprintf("output_%d.log", pid))
-	// outfile, err := os.CreateTemp("", "output_*.log")
+func mainProcess(processData string) (err error) {
+	outHash := hex.EncodeToString(sha3.SumSHAKE256([]byte(processData), 3)) // short hash for identification
+
+	err = os.MkdirAll(logsDir, 0o755)
 	if err != nil {
-		panic(err)
+		return
+	}
+
+	// create file if it does not exist
+	outfile, err := os.OpenFile(filepath.Join(logsDir, outHash+"_out"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o644)
+	if err != nil {
+		return
 	}
 	defer outfile.Close()
 
-	errfile, err := os.Create(fmt.Sprintf("error_%d.log", pid))
-	// errfile, err := os.CreateTemp("", "error_*.log")
+	errfile, err := os.OpenFile(filepath.Join(logsDir, outHash+"_err"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o644)
 	if err != nil {
-		panic(err)
+		return
 	}
-	defer errfile.Close()
+	defer outfile.Close()
 
 	p := &os.ProcAttr{
 		Files: []*os.File{nil, outfile, errfile},
@@ -31,24 +37,30 @@ func cmd() {
 
 	// start a duplicate of the current process
 	name := os.Args[0]
-	fmt.Println("Starting a new process...", os.Args)
+	fmt.Println("Forking current process...", name)
 
-	proc, err := os.StartProcess(name, []string{name, "start"}, p)
+	proc, err := os.StartProcess(name, []string{name, "start", processData}, p)
 	if err != nil {
-		panic(err)
+		return
 	}
 
 	fmt.Println("Started process with PID:", proc.Pid)
+	return
+}
+
+func subProcess() {
 }
 
 func main() {
 	if len(os.Args) > 1 && os.Args[1] == "start" {
-		time.Sleep(2 * time.Second)
-		fmt.Println("This is the child process.")
+		fmt.Println("This is the subprocess.")
+		subProcess()
 		return
 	}
 
-	fmt.Println("This is the parent process.")
-	cmd()
-	fmt.Println("Parent process finished.")
+	fmt.Println("This is the main process.")
+	err := mainProcess("test")
+	if err != nil {
+		fmt.Println("Error starting main process:", err)
+	}
 }
